ed5183c321c52555e7328c89235131c8dbba27e8
SHAFIN AHMED <shafin.ahmed.bd.967@gmail.com>
2026-07-21 10:30:36 +0600

    Rotation

---
+++ Code/Lab Day 08/Rotation/main.cpp
@@ -0,0 +1,123 @@
+#include <GL/glut.h>
+#include <iostream>
+using namespace std;
+
+float X1, Y1, Z1;
+float X2, Y2, Z2;
+
+float angleX = 0.0f;
+float angleY = 0.0f;
+float angleZ = 0.0f;
+
+
+float stepX, stepY, stepZ;
+
+void drawCubeFromPoints(float x1, float y1, float z1,
+                        float x2, float y2, float z2)
+{
+    glBegin(GL_QUADS);
+
+    // Front face
+    glColor3f(1, 0, 0);
+    glVertex3f(x1, y1, z1);
+    glVertex3f(x2, y1, z1);
+    glVertex3f(x2, y2, z1);
+    glVertex3f(x1, y2, z1);
+
+    // Back face
+    glColor3f(0, 1, 0);
+    glVertex3f(x1, y1, z2);
+    glVertex3f(x1, y2, z2);
+    glVertex3f(x2, y2, z2);
+    glVertex3f(x2, y1, z2);
+
+    // Left face
+    glColor3f(0, 0, 1);
+    glVertex3f(x1, y1, z1);
+    glVertex3f(x1, y1, z2);
+    glVertex3f(x1, y2, z2);
+    glVertex3f(x1, y2, z1);
+
+    // Right face
+    glColor3f(1, 1, 0);
+    glVertex3f(x2, y1, z1);
+    glVertex3f(x2, y1, z2);
+    glVertex3f(x2, y2, z2);
+    glVertex3f(x2, y2, z1);
+
+    // Top face
+    glColor3f(0, 1, 1);
+    glVertex3f(x1, y2, z1);
+    glVertex3f(x1, y2, z2);
+    glVertex3f(x2, y2, z2);
+    glVertex3f(x2, y2, z1);
+
+    // Bottom face
+    glColor3f(1, 0, 1);
+    glVertex3f(x1, y1, z1);
+    glVertex3f(x2, y1, z1);
+    glVertex3f(x2, y1, z2);
+    glVertex3f(x1, y1, z2);
+
+    glEnd();
+}
+
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+    gluLookAt(15, 15, 25, 0, 0, 0, 0, 1, 0);
+    glRotatef(angleX, 1, 0, 0);
+    glRotatef(angleY, 0, 1, 0);
+    glRotatef(angleZ, 0, 0, 1);
+    drawCubeFromPoints(X1, Y1, Z1, X2, Y2, Z2);
+    glutSwapBuffers();
+}
+
+void keyboard(unsigned char key, int, int)
+{
+    switch (key)
+    {
+        case 'X': angleX += stepX; break; //rotate +X
+        case 'x': angleX -= stepX; break; //rotate -X
+
+        case 'Y': angleY += stepY; break; //rotate +Y
+        case 'y': angleY -= stepY; break; //rotate -Y
+
+        case 'Z': angleZ += stepZ; break; //rotate +Z
+        case 'z': angleZ -= stepZ; break; //rotate -Z
+    }
+
+    glutPostRedisplay();
+}
+
+void init()
+{
+    glEnable(GL_DEPTH_TEST);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(60, 1.0, 1.0, 100.0);
+    glMatrixMode(GL_MODELVIEW);
+}
+
+int main(int argc, char** argv)
+{
+    cout << "Enter first point (x1 y1 z1): ";
+    cin >> X1 >> Y1 >> Z1;
+
+    cout << "Enter second point (x2 y2 z2): ";
+    cin >> X2 >> Y2 >> Z2;
+
+    cout << "Enter rotation angle steps (stepX stepY stepZ): ";
+    cin >> stepX >> stepY >> stepZ;
+
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(600, 600);
+    glutCreateWindow("3D Rotation With User-Defined Angle Steps");
+    init();
+    glutDisplayFunc(display);
+    glutKeyboardFunc(keyboard);
+    glutMainLoop();
+    return 0;
+}
